3e47224d818b780dc3898c3d1b45cf476eb04209
liufei <clim111@163.com>
2018-07-26 17:39:36 +0800

    e

---
+++ NetNote/Model/BasicUser.cs
@@ -0,0 +1,12 @@
+﻿namespace NetNote
+{
+    public class BasicUser
+    {
+        public BasicUser()
+        {
+        }
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
